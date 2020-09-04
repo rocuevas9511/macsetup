623fe864e524e8e7c0ba28372a33d1595b857f64
rocuevas9511 <30629011+rocuevas9511@users.noreply.github.com>
2020-09-03 22:13:15 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+brew cask install alacritty
+brew cask install visual-studio-code
+brew install tldr
+brew install tmux
+brew install terraform
